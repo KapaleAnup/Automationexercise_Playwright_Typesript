c5465fa532726170772b315d97a9b08f7d7ea7c2
KapaleAnup <kapale.anup@gmail.com>
2024-01-03 10:24:27 +0530

    registration and login page test cases added

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,10 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  testMatch: [
+    // "tests/registrationPage.test.ts"
+    "tests/loginPage.test.ts"
+  ],
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -24,10 +28,10 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://automationexercise.com',
+    //baseURL: 'https://automationexercise.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
@@ -37,15 +41,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
---
+++ tests/loginPage.test.ts
@@ -0,0 +1,21 @@
+import { test } from "../src/basepage/basepage"
+import * as loginData from "../Data/valid_login_creds.json"
+import { expect } from "@playwright/test"
+
+test.beforeEach(async ({ page }) => {
+    await page.goto("https://automationexercise.com")
+})
+
+test.describe('Verify the user is able to login', () => {
+
+    test('Validate login flow with valid credentials', async ({ loginPage, homePage, registrationPage }) => {
+
+        await homePage.clickonSign_Login()
+        await loginPage.getLoginHeaderText()
+        await loginPage.doLogin(loginData.email, loginData.password)
+
+        let userName = await homePage.getUserName()
+
+
+    })
+})
